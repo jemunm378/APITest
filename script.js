408ba6b28484388354475819158879ebbaa9ef04
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-07-17 14:14:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/news").then(res => res.json()).then(console.log)
+fetch("https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/news").then(res => res.json()).then((data) => {document.write(JSON.stringify(data))})
