622d1f46ea235b0cbd686d072c7282eb69d22c4b
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-07-17 19:39:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-fetch("https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/proxy/featured")
-.then(res => res.json())
-.then(text => {
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(text, "text/html");
-    const section = htmlDocument.documentElement.querySelector("section");
-    document.querySelector("div").appendChild(section);
-.then((data) => {document.write(JSON.stringify(data))})
+fetch("https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/proxy/featured").then(res => res.json()).then((data) => {document.write(JSON.stringify(data))})
