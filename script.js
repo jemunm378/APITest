acb177b6181aa821f82454e013e662c3f5d30559
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-07-18 22:29:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/proxy/featured").then(res => res.json()).then((data) => {document.write(JSON.stringify(data))})
+fetch("https://cors-anywhere.herokuapp.com/https://scratch.mit.edu/site-api/comments/user/winter58/").then(res => res.json()).then((data) => {document.write(JSON.stringify(data))})
