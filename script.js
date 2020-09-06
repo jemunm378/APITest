8baca4a56f5317c5898d47cfde5477b615c3ac27
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-09-06 16:12:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://scratchdb.lefty.one/v2/user/info/jemunm").then(res => res.json()).then((data) => { document.getElementById("test").innerHTML = "test " + data.following; });
+fetch("https://scratchdb.lefty.one/v2/user/info/jemunm").then(res => res.json()).then((data) => { document.getElementById("test").innerHTML = "test" + data.following; });
