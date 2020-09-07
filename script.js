7c699fdefa9c654f2e674307a8030b14305d355b
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-09-07 09:58:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 fetch("https://scratchdb.lefty.one/v2/project/info/id/13727").then(res => res.json()).then((data) => { document.getElementById("Title:").innerHTML = "Title:" + data.title; });
+<iframe src="https://forkphorus.github.io/embed.html?id=13727&auto-start=true&light-content=false" width="482" height="393" allowfullscreen="true" allowtransparency="true" style="border:none;"></iframe>
