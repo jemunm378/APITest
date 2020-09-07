0d41e498c626075b1ff88b2d5d302586c1dce2b4
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-09-07 09:44:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://scratchdb.lefty.one/v2/project/info/id/13727").then(res => res.json()).then((data) => { document.getElementById("Title").innerHTML = "Title" + data.title; });
+fetch("https://scratchdb.lefty.one/v2/project/info/id/13727").then(res => res.json()).then((data) => { document.getElementById("Title:").innerHTML = "Title:" + data.title; });
